bdf185c8c55651947f2f11cccc46dc927c0d86ef
211010student <148313537+211010student@users.noreply.github.com>
2023-11-29 14:56:51 +0800

    Update s211010.py

--- student_modules/s211010.py
+++ student_modules/s211010.py
@@ -1,11 +1,13 @@
 def process(message: str) -> None:
     if message == "debug":
         return message
-    if message == "資訊社":
-        return "讚"
-    if message == "資訊老師":
-        return "帥"
-    if message == "資訊社的同學":
-        return "棒"
-    if message == "資訊社的電腦":
-        return "不好說"
+    if message == "指令方塊":
+        return "command block"
+    if message == "透明方塊":
+        return "barrier"
+    if message == "原神":
+        return "啟動"
+    if message == "傳送":
+        return "tp"
+    if message == "死亡":
+        return "kill"
