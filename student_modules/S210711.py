eb302f8101b460b85062e373f2dc2f209050c7db
Wayne821 <148313182+Wayne821@users.noreply.github.com>
2023-12-06 14:00:02 +0800

    Update S210711.py

--- student_modules/S210711.py
+++ student_modules/S210711.py
@@ -1,4 +1,4 @@
-def process(message: str) -> None:
+def process(message: str) -> str:
     if message == "debug":
         return message
     if message == "資訊社":
@@ -9,3 +9,20 @@ def process(message: str) -> None:
         return "棒"
     if message == "資訊社的電腦":
         return "不好說"
+
+    # New rule: Check if the message starts with "計算"
+    if message.startswith("計算"):
+        try:
+            # Extract the mathematical expression and evaluate it
+            calculation_result = eval(message[2:])
+            return str(calculation_result)
+        except Exception as e:
+            return f"計算錯誤: {str(e)}"
+
+    # Add a default return statement if none of the conditions match
+    return "未知訊息"
+
+# Example usage:
+result = process("計算3 + 5")
+print(result)
+
