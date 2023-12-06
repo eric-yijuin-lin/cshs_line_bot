05b26b8df821ebb20258fd241f39f70f07ccb22f
110421student <148313301+110421student@users.noreply.github.com>
2023-12-06 13:53:13 +0800

    Update s110421.py

--- student_modules/s110421.py
+++ student_modules/s110421.py
@@ -1,15 +1,22 @@
-def process(message: str) -> None:
+def process(message: str) -> str:
     if message == "debug":
         return message
-    if message == "資訊社":
+    elif message == "資訊社":
         return "讚"
-    if message == "資訊老師":
+    elif message == "資訊老師":
         return "帥"
-    if message == "資訊社的同學":
+    elif message == "資訊社的同學":
         return "棒"
-    if message == "資訊社的電腦":
+    elif message == "資訊社的電腦":
         return "不好說"
-    if message == "原神":
+    elif message == "原神":
         return "啟動!!!"
-    if message == "今年多大了":
+    elif message == "今年多大了":
         return "剛滿18歲~"
+    else:
+        return "不理解你在說什麼"
+
+# 測試函數
+test_message = "資訊社的同學"
+result = process(test_message)
+print(result)
